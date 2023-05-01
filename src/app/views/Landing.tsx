31223a0cd1e9bc4b9db66bd2ca2819b5fd4f2d58
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-05-01 13:17:19 -0500

    fix bug'

--- src/app/views/Landing.tsx
+++ src/app/views/Landing.tsx
@@ -201,19 +201,7 @@ export default function LandingRoute(){
        
       ]
  
-      function isInViewport(element: HTMLElement | null): boolean {
-        if (!element) {
-          return false;
-        }
-      
-        const rect = element.getBoundingClientRect();
-        return (
-          rect.top >= 0 &&
-          rect.left >= 0 &&
-          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-      }
+ 
       
  
  
