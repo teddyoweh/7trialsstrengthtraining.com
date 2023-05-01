66486d8fc124f01792d28b7015dd59227390b429
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-05-01 12:48:39 -0500

    fix nav bug

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -8,30 +8,7 @@ export default function Nav({open,toggle}){
     return (
         <>
 
-        <nav className={landingstyles.nav}>
-        <div className={landingstyles.logo}>
-            <img src={logo.src} alt="" />
-        </div>
-        <div className={landingstyles.navlinks}>
-            <a href="" className={landingstyles.navactive}>Home</a>
-            <a href="">About</a>
-            <a href="">Our Trainer</a>
-            <a href="">Why Us</a>
-            <a href="">Plans</a>
-            <a href="">Contact</a>
-        </div>
-
-        <div className={landingstyles.navbtns}>
-            <a href="">
-                Get Started
-            </a>
-
-        </div>
-        <div className={landingstyles.menu_icon}>
-        <i className='bx bx-menu' onClick={()=>toggle()}></i>
-        </div>
-
-    </nav>
+    
        {
         open!=null&&
    
